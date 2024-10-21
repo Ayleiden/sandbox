12201ea9458e0564ae818c2659280550b8160678
Ayleiden <renner.sven@gmail.com>
2024-10-21 08:58:00 +0200

    changes

--- chinese_app/chinese_app/settings.py
+++ chinese_app/chinese_app/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "chinese_hsk"
+    "chinese_hsk",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
